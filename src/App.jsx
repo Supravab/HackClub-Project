e278cc81ccf0066b6d3570dec54f2fd786138c6a
Rijan Gautam <rijangautam07@gmail.com>
2025-09-28 23:23:03 +0545

    app.js protected routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import VerifyOtp from './pages/verifyOtp'
+import ProtectedRoute from './routes/protectedroutes'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/otp" element={<VerifyOtp />} />
         <Route path='*' element={<Login />} />
+        {/* Protected Routes--->  */}
       </Routes>
     </Router>
   )
